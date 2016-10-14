1185df362b9b3cb7262920e02df3480962a85f1d
PhilKunz <github@philkunz.com>
2016-10-15 01:06:36 +0200

    update test file

--- test/test.ts
+++ test/test.ts
@@ -1,37 +1,36 @@
-import "typings-test";
+import 'typings-test'
 
-import smartcli = require("../dist/index");
-let beautylog = require("beautylog");
-let should = require("should");
+import smartcli = require('../dist/index')
+let beautylog = require('beautylog')
+let should = require('should')
 
-describe("smartcli.Smartcli class",function(){
-    let smartCliTestObject:smartcli.Smartcli;
-    describe("new Smartcli()",function(){
-        it("should create a new Smartcli",function(){
-            smartCliTestObject = new smartcli.Smartcli();
-            smartCliTestObject.should.be.instanceof(smartcli.Smartcli);
-        });
-    });
-    describe(".addCommand",function(){
-        it("should add an command",function(){
+describe('smartcli.Smartcli class',function(){
+    let smartCliTestObject:smartcli.Smartcli
+    describe('new Smartcli()',function(){
+        it('should create a new Smartcli',function(){
+            smartCliTestObject = new smartcli.Smartcli()
+            smartCliTestObject.should.be.instanceof(smartcli.Smartcli)
+        })
+    })
+    describe('.addCommand',function(){
+        it('should add an command',function(){
             smartCliTestObject.addCommand({
-                commandName:"awesome"
-            });
-            
-        });
-    });
-    describe(".standardTask",function(){
-        it("should start parsing a standardTask",function(done){
+                commandName:'awesome'
+            })
+        })
+    })
+    describe('.standardTask',function(){
+        it('should start parsing a standardTask',function(done){
             smartCliTestObject.standardTask()
                 .then(() => {
-                    console.log("this is the standard Task!");
-                });
-            done();
+                    console.log('this is the standard Task!')
+                })
+            done()
         })
     })
-    describe(".startParse",function(){
-        it("should start parsing the CLI input",function(){
-            smartCliTestObject.startParse();
+    describe('.startParse',function(){
+        it('should start parsing the CLI input',function(){
+            smartCliTestObject.startParse()
         })
     })
-});
+})
